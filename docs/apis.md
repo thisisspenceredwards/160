fb52d676bd83f2fa47df4c4b6146d574f4be9215
FrankWang <powerw@cisco.com>
2019-09-27 15:44:44 -0700

    update apis.md

--- docs/apis.md
+++ docs/apis.md
@@ -11,21 +11,91 @@ Endpoint: api/organization
 Response format: JSON
 
 ## Methods
-* PUT Create a new organization
+* POST Create a new organization [Frank]
 * POST Edit an existing organization
-* GET Search for one or more organizations
+* GET Search for one or more organizations [Frank]
 * DELETE Retire an organization
 
 ## Parameters
-
 * name: String
 * location: String
 * address: String
 * description: String
+* domainName: String, // org domain name, eg. "sjsu.edu"
 * establishedDate: Date
 
 ## Examples
+*POST Create a new organization
+// POST method
+// create a Organization 
+// @input @Param name : the Organization name
+// @input @Param location: location
+// @input @Param address:  address
+// @input @Param description:  description
+// @input @Param domainName:  domain name, eg. "sjsu.edu"
+// @Output: "success: true/false"
+// example:
+// url: http://localhost:3001/api/org/
+//{
+//    "name": "San Jose State University",
+//    "location": "San Jose",
+//    "address": "1 frist street, San Jose",
+//    "description": "Nice college",
+//    "domainName": "sjsu.edu"
+//}
 
+* GET Search for one or more organizations
+// GET method
+// get the org list
+// @input @Param OrgId : the org ID
+// Output: the org info or the list of all org info if OrgId is empty
+/*
+example -- get all org info
+url: http://localhost:3001/api/org
+output JSON:
+[
+    {
+        "establishedDate": "2019-09-27T21:37:32.862Z",
+        "_id": "5d8e815b6854909ffb0f5755",
+        "name": "San Jose State University",
+        "location": "San Jose",
+        "address": "1 first street, San Jose",
+        "description": "Nice college",
+        "domainName": "sjsu.edu",
+        "createdAt": "2019-09-27T21:38:35.714Z",
+        "updatedAt": "2019-09-27T21:38:35.714Z",
+        "__v": 0
+    },
+    {
+        "establishedDate": "2019-09-27T21:59:37.438Z",
+        "_id": "5d8e86d5ac3936a0e34708af",
+        "name": "Santa Clara University",
+        "location": "Santa Clara",
+        "address": "1 first street, Santa Clara",
+        "description": "Nice college",
+        "domainName": "scu.edu",
+        "createdAt": "2019-09-27T22:01:57.499Z",
+        "updatedAt": "2019-09-27T22:01:57.499Z",
+        "__v": 0
+    }
+]
+example two -- get one org info given by orgId
+url http://localhost:3001/api/org?orgId=5d8e86d5ac3936a0e34708af
+[
+    {
+        "establishedDate": "2019-09-27T21:59:37.438Z",
+        "_id": "5d8e86d5ac3936a0e34708af",
+        "name": "Santa Clara University",
+        "location": "Santa Clara",
+        "address": "1 first street, Santa Clara",
+        "description": "Nice college",
+        "domainName": "scu.edu",
+        "createdAt": "2019-09-27T22:01:57.499Z",
+        "updatedAt": "2019-09-27T22:01:57.499Z",
+        "__v": 0
+    }
+]
+*/
 
 # Users
 
---
+++ fullstack_app/backend/OrgAPI.js
@@ -0,0 +1,138 @@
+const express = require('express')
+const { check, validationResult } = require('express-validator')
+const models = require('./data')
+const authenticationFile = require('./authenticationClass')
+const constants = require('./constants')
+const router = express.Router()
+const Organization = models.Organization
+
+// POST method
+// create a Organization 
+// @input @Param name : the Organization name
+// @input @Param location: location
+// @input @Param address:  address
+// @input @Param description:  description
+// @input @Param domainName:  domain name, eg. "sjsu.edu"
+// @Output: "success: true/false"
+// example:
+// url: http://localhost:3001/api/org/
+//{
+//    "name": "San Jose State University",
+//    "location": "San Jose",
+//    "address": "1 frist street, San Jose",
+//    "description": "Nice college",
+//    "domainName": "sjsu.edu"
+//}
+
+router.post('/org', [
+    check('name').isLength({ min:1 }).withMessage('The name has to be non-empty.'),
+    check('location').isLength({ min:1 }).withMessage('The location has to be non-empty.'),
+    check('address').isLength({ min:1 }).withMessage('The address has to be non-empty.'),
+    check('description').isLength({ min:1 }).withMessage('The description has to be non-empty.'),
+    check('domainName').isLength({ min:1 }).withMessage('The domainName has to be non-empty.'),
+    ], async (req, res) =>
+{
+    const errors = validationResult(req)
+    if (!errors.isEmpty())
+    {
+        return res.status(422).json(errors) // must work on this
+    }
+
+    let organization = new Organization()
+    const {name, location, address, description, domainName } = req.body
+
+    const query = {};
+    query.domainName = domainName;    
+    //check if a duplicated item based on the domain name
+    Organization.findOne( query, '', (err, org) => {
+        if (err) {
+            return res.send(err);
+        } else {
+            if(org != null) {
+                // find a duplicated domain name, don't save
+                //console.log(org);
+                return res.json(constants.DUPLICATED_ORG_JSDON);
+            } else {
+                organization.name = name
+                organization.location = location
+                organization.address = address
+                organization.description = description
+                organization.domainName = domainName
+                // save it
+                organization.save((err) =>
+                {
+                    if (err) return res.json(constants.FAIL_JSON)
+                    else return res.json(constants.SUCCESS_JSON)
+                })
+            }
+        }
+    });
+ })
+
+// GET method
+// get the org list
+// @input @Param OrgId : the org ID
+// Output: the org info or the list of all org info if OrgId is empty
+/*
+example
+url: http://localhost:3001/api/org
+output JSON:
+[
+    {
+        "establishedDate": "2019-09-27T21:37:32.862Z",
+        "_id": "5d8e815b6854909ffb0f5755",
+        "name": "San Jose State University",
+        "location": "San Jose",
+        "address": "1 first street, San Jose",
+        "description": "Nice college",
+        "domainName": "sjsu.edu",
+        "createdAt": "2019-09-27T21:38:35.714Z",
+        "updatedAt": "2019-09-27T21:38:35.714Z",
+        "__v": 0
+    },
+    {
+        "establishedDate": "2019-09-27T21:59:37.438Z",
+        "_id": "5d8e86d5ac3936a0e34708af",
+        "name": "Santa Clara University",
+        "location": "Santa Clara",
+        "address": "1 first street, Santa Clara",
+        "description": "Nice college",
+        "domainName": "scu.edu",
+        "createdAt": "2019-09-27T22:01:57.499Z",
+        "updatedAt": "2019-09-27T22:01:57.499Z",
+        "__v": 0
+    }
+]
+example two
+url http://localhost:3001/api/org?orgId=5d8e86d5ac3936a0e34708af
+[
+    {
+        "establishedDate": "2019-09-27T21:59:37.438Z",
+        "_id": "5d8e86d5ac3936a0e34708af",
+        "name": "Santa Clara University",
+        "location": "Santa Clara",
+        "address": "1 first street, Santa Clara",
+        "description": "Nice college",
+        "domainName": "scu.edu",
+        "createdAt": "2019-09-27T22:01:57.499Z",
+        "updatedAt": "2019-09-27T22:01:57.499Z",
+        "__v": 0
+    }
+]
+*/
+ router.get('/org', [], async(req, res) =>
+{
+    const query = {};
+    if (req.query.orgId) {
+        query._id = req.query.orgId;
+    }
+    console.log(query._id);
+    Organization.find(query, '', (err, org) => {
+        if (err) {
+            return res.send(err);
+        } else {
+            return res.json(org);
+        }
+    });
+});
+module.exports = router
